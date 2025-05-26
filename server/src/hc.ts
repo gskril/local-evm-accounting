f56f48626922e99e2111fd232da9059ffc040029
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-05-26 01:19:15 -0400

    Make `vite preview` work

--- server/src/hc.ts
+++ server/src/hc.ts
@@ -5,6 +5,3 @@ import { routes } from './api'
 // Assign the client to a variable to calculate the type when compiling
 export const honoClient = hc<typeof routes>('')
 export type Client = typeof honoClient
-
-export const hcWithType = (...args: Parameters<typeof hc>): typeof honoClient =>
-  hc<typeof routes>(...args)
